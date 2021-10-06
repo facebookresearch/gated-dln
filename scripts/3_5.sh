ef1b4be41e38b3d7b388f6bc9b659e522509e9b5
Shagun Sodhani <sodhani@fb.com>
2021-10-06 07:38:10 -0700

    Added different scripts

---
+++ scripts/3_5.sh
@@ -0,0 +1,26 @@
+python3 -u four_path_model.py -m \
+setup.git.issue_id=3-1 \
+dataloader=cifar10_v1 \
+experiment.task_one.name=living_or_not \
+experiment.task_one.transform=default \
+experiment.task_two.name=fly_or_not \
+experiment.task_two.transform=default \
+experiment=four_path_model \
+model.num_layers=1,2 \
+model.hidden_layer_cfg.dim=2048,4096 \
+model.should_use_non_linearity=False \
+model.encoder_cfg.should_share=False \
+model.hidden_layer_cfg.should_share=True \
+model.decoder_cfg.should_share=False \
+setup.script_id=3-5 \
+model.weight_init.should_do=True \
+model.weight_init.gain=1.0,0.1,0.01,0.001,0.0001 \
+model.weight_init.bias=0,0.1,0.01,0.001 \
+optimizer=adam \
+optimizer.lr=0.001 \
+dataloader.task_specific_cfgs.living_or_not.train_config.model.should_use_pretrained=True \
+dataloader.task_specific_cfgs.living_or_not.test_config.model.should_use_pretrained=True \
+dataloader.task_specific_cfgs.fly_or_not.train_config.model.should_use_pretrained=True \
+dataloader.task_specific_cfgs.fly_or_not.test_config.model.should_use_pretrained=True \
+'setup.viz.params=[experiment.task_one.name,experiment.task_one.transform,experiment.task_two.name,experiment.task_two.transform,experiment.name,model.num_layers,model.hidden_layer_cfg.dim,model.should_use_non_linearity,model.encoder_cfg.should_share,model.hidden_layer_cfg.should_share,model.decoder_cfg.should_share,model.weight_init.should_do,model.weight_init.gain,model.weight_init.bias,optimizer.lr,optimizer._target_,dataloader.task_specific_cfgs.living_or_not.train_config.model.should_use_pretrained,dataloader.task_specific_cfgs.living_or_not.test_config.model.should_use_pretrained,dataloader.task_specific_cfgs.fly_or_not.train_config.model.should_use_pretrained,dataloader.task_specific_cfgs.fly_or_not.test_config.model.should_use_pretrained]' \
+'setup.description=experiment.task_one.name-${experiment.task_one.name}----experiment.task_one.transform-${experiment.task_one.transform}----experiment.task_two.name-${experiment.task_two.name}----experiment.task_two.transform-${experiment.task_two.transform}----experiment.name-${experiment.name}----model.num_layers-${model.num_layers}----model.hidden_layer_cfg.dim-${model.hidden_layer_cfg.dim}----model.should_use_non_linearity-${model.should_use_non_linearity}----model.encoder_cfg.should_share-${model.encoder_cfg.should_share}----model.hidden_layer_cfg.should_share-${model.hidden_layer_cfg.should_share}----model.decoder_cfg.should_share-${model.decoder_cfg.should_share}----model.weight_init.should_do-${model.weight_init.should_do}----model.weight_init.gain-${model.weight_init.gain}----model.weight_init.bias-${model.weight_init.bias}----optimizer.lr-${optimizer.lr}----optimizer._target_-${optimizer._target_}----dataloader.task_specific_cfgs.living_or_not.train_config.model.should_use_pretrained-${dataloader.task_specific_cfgs.living_or_not.train_config.model.should_use_pretrained}----dataloader.task_specific_cfgs.living_or_not.test_config.model.should_use_pretrained-${dataloader.task_specific_cfgs.living_or_not.test_config.model.should_use_pretrained}----dataloader.task_specific_cfgs.fly_or_not.train_config.model.should_use_pretrained-${dataloader.task_specific_cfgs.fly_or_not.train_config.model.should_use_pretrained}----dataloader.task_specific_cfgs.fly_or_not.test_config.model.should_use_pretrained-${dataloader.task_specific_cfgs.fly_or_not.test_config.model.should_use_pretrained}'
---
+++ scripts/7_5.sh
@@ -0,0 +1,22 @@
+python3 -u k_path_model.py -m \
+setup.git.issue_id=7 \
+experiment.num_epochs=1000 \
+model.gate_cfg.mode=16_plus_mod,20_plus_mod,12_plus_mod \
+experiment.task.num_classes_in_original_dataset=8 \
+dataloader.train_config.dataloader.batch_size=16,8 \
+experiment=k_path_model \
+model.num_layers=1 \
+model.hidden_layer_cfg.dim=128,1024,2048 \
+model.should_use_non_linearity=False \
+model.encoder_cfg.should_share=False \
+model.hidden_layer_cfg.should_share=True \
+model.decoder_cfg.should_share=False \
+setup.script_id=7-5 \
+model.weight_init.should_do=True \
+model.weight_init.gain=1.0,0.1,0.01,0.001,0.0001 \
+model.weight_init.bias=0,0.01 \
+optimizer=sgd \
+optimizer.lr=0.0001,0.001,0.01 \
+optimizer.momentum=0.9 \
+'setup.viz.params=[experiment.num_epochs,model.gate_cfg.mode,experiment.task.num_classes_in_original_dataset,dataloader.train_config.dataloader.batch_size,experiment.name,model.num_layers,model.hidden_layer_cfg.dim,model.should_use_non_linearity,model.encoder_cfg.should_share,model.hidden_layer_cfg.should_share,model.decoder_cfg.should_share,model.weight_init.should_do,model.weight_init.gain,model.weight_init.bias,optimizer._target_,optimizer.lr,optimizer.momentum]' \
+'setup.description=experiment.num_epochs-${experiment.num_epochs}----model.gate_cfg.mode-${model.gate_cfg.mode}----experiment.task.num_classes_in_original_dataset-${experiment.task.num_classes_in_original_dataset}----dataloader.train_config.dataloader.batch_size-${dataloader.train_config.dataloader.batch_size}----experiment.name-${experiment.name}----model.num_layers-${model.num_layers}----model.hidden_layer_cfg.dim-${model.hidden_layer_cfg.dim}----model.should_use_non_linearity-${model.should_use_non_linearity}----model.encoder_cfg.should_share-${model.encoder_cfg.should_share}----model.hidden_layer_cfg.should_share-${model.hidden_layer_cfg.should_share}----model.decoder_cfg.should_share-${model.decoder_cfg.should_share}----model.weight_init.should_do-${model.weight_init.should_do}----model.weight_init.gain-${model.weight_init.gain}----model.weight_init.bias-${model.weight_init.bias}----optimizer._target_-${optimizer._target_}----optimizer.lr-${optimizer.lr}----optimizer.momentum-${optimizer.momentum}'
