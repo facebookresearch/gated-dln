54f41b1d39e6e728f323d732c7af2ea3e6adb039
Shagun Sodhani <sodhani@fb.com>
2021-10-15 18:58:01 -0700

    Add moe based decoder

--- src/model/k_path_model.py
+++ src/model/k_path_model.py
@@ -14,6 +14,8 @@ from src.experiment.ds import ExperimentMetadata, TasksForKPathModel
 from src.model import utils as model_utils
 from src.model.base import Model as BaseModelCls
 
+USE_MOE = True
+
 
 class BaseModel(BaseModelCls):
     def __init__(
@@ -32,6 +34,10 @@ class BaseModel(BaseModelCls):
         super().__init__(name=name, model_cfg=None, description=description)  # type: ignore[arg-type]
         # error: Argument "model_cfg" to "__init__" of "Model" has incompatible type "None"; expected "DictConfig"
 
+        assert encoder_cfg["should_share"] is False
+
+        assert decoder_cfg["should_share"] is False
+
         self.tasks = tasks
 
         hidden_size = hidden_layer_cfg["dim"]
@@ -47,17 +53,28 @@ class BaseModel(BaseModelCls):
                 for _ in range(self.tasks.shape[0])
             ]
         )
-        self.decoders = nn.ModuleList(
-            [
-                model_utils.get_decoder(
-                    out_features=self.tasks.out_features,
-                    num_layers=num_layers,
-                    hidden_size=hidden_size,
-                    should_use_non_linearity=should_use_non_linearity,
-                )
-                for _ in range(self.tasks.shape[1])
-            ]
-        )
+
+        if USE_MOE:
+            self.decoders = model_utils.get_moe_decoder(
+                num_experts=self.tasks.shape[0],
+                out_features=self.tasks.out_features,
+                num_layers=num_layers,
+                hidden_size=hidden_size,
+                should_use_non_linearity=should_use_non_linearity,
+            )
+        else:
+            self.decoders = nn.ModuleList(
+                [
+                    model_utils.get_decoder(
+                        out_features=self.tasks.out_features,
+                        num_layers=num_layers,
+                        hidden_size=hidden_size,
+                        should_use_non_linearity=should_use_non_linearity,
+                    )
+                    for _ in range(self.tasks.shape[1])
+                ]
+            )
+
         if weight_init["should_do"]:
             init_weights = model_utils.get_weight_init_fn(
                 gain=weight_init["gain"], bias=weight_init["bias"]
@@ -67,9 +84,10 @@ class BaseModel(BaseModelCls):
         self.encoders = torch.jit.script(self.encoders)
         self.decoders = torch.jit.script(self.decoders)
 
-        assert encoder_cfg["should_share"] is False
-
-        assert decoder_cfg["should_share"] is False
+        if USE_MOE:
+            self.get_decoder_output = self.get_decoder_output_using_moe
+        else:
+            self.get_decoder_output = self.get_decoder_output_using_model_list
 
         self.loss_fn = nn.CrossEntropyLoss(reduction="none")
 
@@ -188,20 +206,25 @@ class Model(BaseModel):
 
         assert hidden_layer_cfg["should_share"] is True
 
-    def forward(
-        self, x: torch.Tensor, y: torch.Tensor, metadata: ExperimentMetadata
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-
-        batch_size = x.shape[0]
-
-        transformed_x = [transform(x) for transform in self.tasks.input_transforms]
-        features = [encoder(x) for encoder, x in zip(self.encoders, transformed_x)]
-
-        hidden = self.hidden_layer(
-            torch.cat(features, dim=1).view(-1, features[0].shape[1])
+    def get_decoder_output_using_moe(
+        self, hidden: torch.Tensor, batch_size: int
+    ) -> torch.Tensor:
+        outputs = self.decoders(hidden)
+        outputs = (
+            outputs.permute(1, 0, 2)
+            # .view(
+            #     batch_size,
+            #     self.tasks.shape[0],
+            #     self.tasks.shape[1],
+            #     self.tasks.out_features,
+            # )
+            .reshape(-1, self.tasks.out_features)
         )
-        # (batch_size * self.tasks.shape[0], dim)
+        return outputs
 
+    def get_decoder_output_using_model_list(
+        self, hidden: torch.Tensor, batch_size: int
+    ) -> torch.Tensor:
         outputs = [
             decoder(hidden)
             .reshape(batch_size, self.tasks.shape[0], self.tasks.out_features)
@@ -209,7 +232,28 @@ class Model(BaseModel):
             for decoder in self.decoders
         ]  # list of size self.tasks.shape[1]
         # outputs[0].shape == (batch, self.tasks.shape[0], 1, 2)
+
         output_tensor = torch.cat(outputs, dim=2).view(-1, self.tasks.out_features)
+        return output_tensor
+
+    def forward(
+        self, x: torch.Tensor, y: torch.Tensor, metadata: ExperimentMetadata
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        batch_size = x.shape[0]
+
+        transformed_x = [transform(x) for transform in self.tasks.input_transforms]
+        features = [
+            encoder(x).unsqueeze(1) for encoder, x in zip(self.encoders, transformed_x)
+        ]
+        hidden = self.hidden_layer(
+            torch.cat(features, dim=1).view(
+                batch_size * self.tasks.shape[0], features[0].shape[2]
+            )
+        )
+        # (batch_size * self.tasks.shape[0], dim)
+
+        output_tensor = self.get_decoder_output(hidden=hidden, batch_size=batch_size)
+
         # (batch, self.tasks.shape[0], self.tasks.shape[1], 2)
 
         transformed_y = [transform(y) for transform in self.tasks.target_transforms]
