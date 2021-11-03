4fb3122acb869d2c1074a4b4d99b7a91f6441ad3
Shagun Sodhani <sodhani@fb.com>
2021-11-03 16:06:26 -0700

    Add top-level script to extract the features

---
+++ extract_feature.py
@@ -0,0 +1,37 @@
+"""This is the main entry point for the code."""
+
+import json
+
+import hydra
+from omegaconf import DictConfig
+
+from src.app.run import run
+from src.config_builder.register import prepare_for_loading_configs
+from src.utils import config as config_utils
+
+config_path = "config"
+config_name = "k_path_model"
+
+prepare_for_loading_configs()
+
+
+@hydra.main(config_path=config_path, config_name=config_name)
+def launch(config: DictConfig) -> None:
+    try:
+        config = config_utils.read_config_from_file_for_resuming(
+            config_path=f"/private/home/sodhani/projects/abstraction_by_gating/logs/{config.setup.id}/config_log.jsonl"
+        )
+    except json.decoder.JSONDecodeError:
+        config = config_utils.read_json_config_from_file(
+            config_path=f"/private/home/sodhani/projects/abstraction_by_gating/logs/{config.setup.id}/config_log.jsonl"
+        )
+
+    config = config_utils.make_config_mutable(config)
+    config.experiment.name = "extract_feature"
+    config.experiment.builder._target_ = "src.experiment.extract_feature.Experiment"
+    config.dataloader.train_config.dataloader.batch_size = 1024
+    return run(config)
+
+
+if __name__ == "__main__":
+    launch()
