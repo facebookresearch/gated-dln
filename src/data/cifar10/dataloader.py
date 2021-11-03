78c354746e943facdb5225480a79b4324430c218
Shagun Sodhani <sodhani@fb.com>
2021-11-03 10:51:47 -0700

    Add CIFAR10 dataloader

---
+++ src/data/cifar10/dataloader.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import warnings
+
+import torch
+from torchvision import transforms as transforms
+
+from src.data.torchvision import dataloader as torchvision_data_utils
+from src.utils.config import DictConfig
+
+
+def build_dataloaders(
+    name: str,
+    train_config: DictConfig,
+    test_config: DictConfig,
+    transform,
+    target_transform,
+) -> dict[str, torch.utils.data.DataLoader]:
+
+    if transform:
+        warnings.warn("transform arg is deprecated", DeprecationWarning, stacklevel=2)
+
+    # todo: remove transform from the function call here.
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    return torchvision_data_utils.build_dataloaders(
+        name=name,
+        train_config=train_config,
+        test_config=test_config,
+        transform=transform,
+        target_transform=target_transform,
+    )
