61dc5980966ac762ef92692dd989c6dd50bc81d5
Shagun Sodhani <sodhani@fb.com>
2022-06-04 17:18:39 -0700

    Replace moe layer by moe package

--- src/model/moe/layer.py
+++ src/model/moe/layer.py
@@ -3,60 +3,9 @@ from __future__ import annotations
 
 import hydra
 import torch
+from moe.model import layer as moe_layer
+from omegaconf import DictConfig
 from torch import nn
-
-
-class Linear(nn.Module):
-    def __init__(
-        self, num_experts: int, in_features: int, out_features: int, bias: bool = True
-    ):
-        """torch.nn.Linear layer extended for use as a mixture of experts.
-
-        Args:
-            num_experts (int): number of experts in the mixture.
-            in_features (int): size of each input sample for one expert.
-            out_features (int): size of each output sample for one expert.
-            bias (bool, optional): if set to ``False``, the layer will
-                not learn an additive bias. Defaults to True.
-        """
-        super().__init__()
-        self.num_experts = num_experts
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = nn.Parameter(
-            torch.rand(self.num_experts, self.in_features, self.out_features)
-        )
-        if bias:
-            self.bias = nn.Parameter(torch.rand(self.num_experts, 1, self.out_features))
-            self.use_bias = True
-        else:
-            self.use_bias = False
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if self.use_bias:
-            return x.matmul(self.weight) + self.bias
-        else:
-            return x.matmul(self.weight)
-
-    def extra_repr(self) -> str:
-        return f"num_experts={self.num_experts}, in_features={self.in_features}, out_features={self.out_features}, bias={self.use_bias}"
-
-
-class FunctionalLinear(nn.Module):
-    def __init__(self):
-        """Functional version of Linear"""
-        super().__init__()
-
-    def forward(
-        self, input: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor
-    ) -> torch.Tensor:
-        # shape of weight is (num_experts, in_features, out_features)
-        # shape of bias is (num_experts, 1, out_features)
-
-        return input.matmul(weight) + bias
-
-    def extra_repr(self) -> str:
-        return "FunctionalLinear layer"
 
 
 class FeedForward(nn.Module):
@@ -87,7 +36,7 @@ class FeedForward(nn.Module):
         layers: list[nn.Module] = []
         current_in_features = in_features
         for _ in range(num_layers - 1):
-            linear = Linear(
+            linear = moe_layer.Linear(
                 num_experts=num_experts,
                 in_features=current_in_features,
                 out_features=hidden_features,
@@ -97,7 +46,7 @@ class FeedForward(nn.Module):
             if should_use_non_linearity:
                 layers.append(hydra.utils.instantiate(non_linearity_cfg))
             current_in_features = hidden_features
-        linear = Linear(
+        linear = moe_layer.Linear(
             num_experts=num_experts,
             in_features=current_in_features,
             out_features=out_features,
